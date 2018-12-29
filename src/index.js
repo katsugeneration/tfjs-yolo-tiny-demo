44998324bbb4168936e53f2092c740b32a4f8a16
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2018-12-29 16:47:05 +0900

    Limit return results

--- src/index.js
+++ src/index.js
@@ -12,82 +12,32 @@ const class_names = [
   'bicycle',
   'car',
   'motorbike',
-  'aeroplane',
-  'bus',
-  'train',
-  'truck',
-  'boat',
-  'traffic light',
-  'fire hydrant',
-  'stop sign',
-  'parking meter',
-  'bench',
   'bird',
   'cat',
   'dog',
-  'horse',
-  'sheep',
-  'cow',
-  'elephant',
-  'bear',
-  'zebra',
-  'giraffe',
   'backpack',
   'umbrella',
   'handbag',
   'tie',
-  'suitcase',
-  'frisbee',
-  'skis',
-  'snowboard',
   'sports ball',
-  'kite',
-  'baseball bat',
-  'baseball glove',
-  'skateboard',
-  'surfboard',
-  'tennis racket',
   'bottle',
-  'wine glass',
   'cup',
   'fork',
   'knife',
   'spoon',
-  'bowl',
   'banana',
   'apple',
-  'sandwich',
   'orange',
-  'broccoli',
-  'carrot',
-  'hot dog',
-  'pizza',
-  'donut',
-  'cake',
   'chair',
   'sofa',
-  'pottedplant',
   'bed',
   'diningtable',
-  'toilet',
-  'tvmonitor',
   'laptop',
   'mouse',
-  'remote',
   'keyboard',
   'cell phone',
-  'microwave',
-  'oven',
-  'toaster',
-  'sink',
-  'refrigerator',
   'book',
   'clock',
-  'vase',
-  'scissors',
-  'teddy bear',
-  'hair drier',
-  'toothbrush',
 ];
 
 for (var c of class_names) {
@@ -116,13 +66,12 @@ async function run() {
 
     const t0 = performance.now();
 
-    const boxes = await yolo(inputImage, model);
+    const boxes = await yolo(inputImage, model, {maxBoxes: 2});
 
     inputImage.dispose();
 
     const t1 = performance.now();
     console.log("YOLO inference took " + (t1 - t0) + " milliseconds.");
-
     console.log('tf.memory(): ', tf.memory());
 
     clearImages();
